e0dd259d42e9fae1bd3f8b4b8f6ae1889e824737
Chris Farmer <farmerc@udel.edu>
2020-03-28 01:07:08 -0400

    finished up to 6.1

---
+++ src/main/resources/import.sql
@@ -0,0 +1,74 @@
+insert into poll (poll_id, question) values (1, 'What is your favorite color?');
+insert into options (option_id, option_value, poll_id) values(1, 'Blue', 1);
+insert into options (option_id, option_value, poll_id) values(2, 'Red', 1);
+insert into options (option_id, option_value, poll_id) values(3, 'Green', 1);
+
+insert into poll (poll_id, question) values (2, 'What is your least favorite color?');
+insert into options (option_id, option_value, poll_id) values(4, 'Pink', 2);
+insert into options (option_id, option_value, poll_id) values(5, 'Yellow', 2);
+insert into options (option_id, option_value, poll_id) values(6, 'Brown', 2);
+
+insert into poll (poll_id, question) values (3, 'Which Pokemon is the first Pokemon in the Pokedex?');
+insert into options (option_id, option_value, poll_id) values(7, 'Bulbasaur', 3);
+insert into options (option_id, option_value, poll_id) values(8, 'Charmander', 3);
+insert into options (option_id, option_value, poll_id) values(9, 'Squirtle', 3);
+
+insert into poll (poll_id, question) values (4, 'Who is Zip Codes Best Instructor?');
+insert into options (option_id, option_value, poll_id) values(10, 'Chris', 4);
+insert into options (option_id, option_value, poll_id) values(11, 'Dolio', 4);
+insert into options (option_id, option_value, poll_id) values(12, 'Sian', 4);
+
+insert into poll (poll_id, question) values (5, 'How tall are you?');
+insert into options (option_id, option_value, poll_id) values(13, 'Below 6 foot', 5);
+insert into options (option_id, option_value, poll_id) values(14, '6 foot', 5);
+insert into options (option_id, option_value, poll_id) values(15, 'Above 6 foot', 5);
+
+insert into poll (poll_id, question) values (6, 'What is your favorite car?');
+insert into options (option_id, option_value, poll_id) values(16, 'Ford', 6);
+insert into options (option_id, option_value, poll_id) values(17, 'Saab', 6);
+insert into options (option_id, option_value, poll_id) values(18, 'Honda', 6);
+
+insert into poll (poll_id, question) values (7, 'What is the worst season of GoT?');
+insert into options (option_id, option_value, poll_id) values(19, 'Season 8', 7);
+insert into options (option_id, option_value, poll_id) values(20, 'Season 8', 7);
+insert into options (option_id, option_value, poll_id) values(21, 'Season 8', 7);
+
+insert into poll (poll_id, question) values (8, 'What is the best class in D&D?');
+insert into options (option_id, option_value, poll_id) values(22, 'Sorcerer', 8);
+insert into options (option_id, option_value, poll_id) values(23, 'Wizard', 8);
+insert into options (option_id, option_value, poll_id) values(24, 'Bard', 8);
+
+insert into poll (poll_id, question) values (9, 'What is the best class in WoW?');
+insert into options (option_id, option_value, poll_id) values(25, 'Shaman', 9);
+insert into options (option_id, option_value, poll_id) values(26, 'Paladin', 9);
+insert into options (option_id, option_value, poll_id) values(27, 'DemonHunter', 9);
+
+insert into poll (poll_id, question) values (10, 'What is the best video game of all time?');
+insert into options (option_id, option_value, poll_id) values(28, 'The Witcher 3', 10);
+insert into options (option_id, option_value, poll_id) values(29, 'Divinity: Original Sin 2', 10);
+insert into options (option_id, option_value, poll_id) values(30, 'World of Warcraft', 10);
+
+insert into poll (poll_id, question) values (11, 'Who is the greatest Eagle of all time?');
+insert into options (option_id, option_value, poll_id) values(31, 'Brian Dawkins', 11);
+insert into options (option_id, option_value, poll_id) values(32, 'Reggie White', 11);
+insert into options (option_id, option_value, poll_id) values(33, 'Steve Van Buren', 11);
+
+insert into poll (poll_id, question) values (12, 'What superpower do you want?');
+insert into options (option_id, option_value, poll_id) values(34, 'Flight', 12);
+insert into options (option_id, option_value, poll_id) values(35, 'Invisibility', 12);
+insert into options (option_id, option_value, poll_id) values(36, 'Invulnerability', 12);
+
+insert into poll (poll_id, question) values (13, 'Which Star Wars movie is the best?');
+insert into options (option_id, option_value, poll_id) values(37, 'The Empire Strikes Back', 13);
+insert into options (option_id, option_value, poll_id) values(38, 'A New Hope', 13);
+insert into options (option_id, option_value, poll_id) values(39, 'Return of the Jedi', 13);
+
+insert into poll (poll_id, question) values (14, 'What is the airspeed velocity of an unladen swallow?');
+insert into options (option_id, option_value, poll_id) values(40, '10', 14);
+insert into options (option_id, option_value, poll_id) values(41, '11', 14);
+insert into options (option_id, option_value, poll_id) values(42, '12', 14);
+
+insert into poll (poll_id, question) values (15, 'Who is the greatest comedian of all time?');
+insert into options (option_id, option_value, poll_id) values(43, 'John Mulaney', 15);
+insert into options (option_id, option_value, poll_id) values(44, 'Dave Chappelle', 15);
+insert into options (option_id, option_value, poll_id) values(45, 'Burt Kreischer', 15);
